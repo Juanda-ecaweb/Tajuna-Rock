655bf1b446114d35295e455a00005282d004088e
Juan David López de la Torre <jdlopez@ecaweb.es>
2026-08-25 19:10:47 +0200

    Cambiar puesto de vinilos a Varios musicales

--- js/datos.js
+++ js/datos.js
@@ -739,10 +739,10 @@
             <p class="product-description">Venta de productos de manualidades y artesanía con una filosofía de crear, reutilizar y restaurar. Damos nuevas oportunidades a objetos, materiales e ideas y ponemos color a la vida.</p>
           </article>
           <article class="product-card">
-            <img src="imagenes/logos/tajuna-rock-logo.png" alt="Vinilos de Tajuña Rock" loading="lazy" />
-            <h4 class="product-name">Vinilos Tajuña Rock</h4>
-            <p class="product-meta">Música en vinilo</p>
-            <p class="product-description">Colección de vinilos con lo mejor del rock nacional e internacional. Descubre bandas clásicas y emergentes en el formato más auténtico. Un must para los melómanos del Tajuña Rock.</p>
+            <img src="imagenes/logos/tajuna-rock-logo.png" alt="Varios musicales" loading="lazy" />
+            <h4 class="product-name">Varios musicales</h4>
+            <p class="product-meta">Artículos musicales</p>
+            <p class="product-description">Cuadros de música, parches, autógrafos, vinilos e imanes.</p>
           </article>
         </div>
 
